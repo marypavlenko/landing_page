670e3fc5b1f9b40ba5e94099b1d09a30a1b28268
Maria <mary.pavlenko.ab@gmail.com>
2019-03-18 20:15:02 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import "./styles/main.pcss";
+if (process.env.NODE_ENV === "development") {
+  require("file-loader!./index.pug");
+}
+
+import "./scripts/skills";
+
+// Mobile menu
+function toggleMenu() {
+  var openBtn = $('.button__burger');
+  var closeBtn = $('.nav-mobile__close-btn');
+  var menu = $('.nav-mobile');
+
+  openBtn.on('click', function (event) {
+    event.preventDefault();
+    menu.fadeIn();
+    $('body').addClass('scroll-hidden');
+  });
+
+  closeBtn.on('click', function (event) {
+    event.preventDefault();
+    menu.fadeOut();
+    $('body').removeClass('scroll-hidden');
+  });
+}
+
+toggleMenu();
