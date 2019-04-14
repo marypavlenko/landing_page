569b4b3a6fce7a1cf949b63a36def1f00a0022f3
mariapavlenko <vstre4KA>
2019-04-14 14:45:35 +0400

    Add action for slider

--- src/main.js
+++ src/main.js
@@ -6,3 +6,4 @@ if (process.env.NODE_ENV === "development") {
 import "./scripts/skills";
 import "./scripts/mobile-menu";
 import "./scripts/parallax";
+import "./scripts/slider";
---
+++ src/scripts/slider.js
@@ -0,0 +1,70 @@
+const slides = [
+    {
+        title: 'title 1',
+        image: '/src/images/content/works/parachute.jpg',
+        tags: ['HTML', 'CSS', 'JavaScript'],
+        description: 'This girl learning web development! 4 months only the most difficult trials and sleepless nights!',
+        link: '#',
+    },
+    {
+        title: 'title 2',
+        image: '/src/images/content/works/valley.jpg',
+        tags: ['HTML', 'CSS', 'JavaScript'],
+        description: 'This girl learning web development! 4 months only the most difficult trials and sleepless nights!',
+        link: '#',
+    },
+    {
+        title: 'title 3',
+        image: '/src/images/content/works/city.jpg',
+        tags: ['HTML', 'CSS', 'JavaScript'],
+        description: 'This girl learning web development! 4 months only the most difficult trials and sleepless nights!',
+        link: '#',
+    }
+];
+
+const slider = document.querySelector('.js-slider');
+const mainSlide = slider.querySelector('.js-main-slide');
+const tags = slider.querySelector('.js-slide-tags');
+const slideTitle = slider.querySelector('.js-slide-title');
+const slideDescription = slider.querySelector('.js-slide-description');
+const slideLink = slider.querySelector('.js-slide-link');
+const upSlide = slider.querySelector('.js-scroll-up');
+const downSlide = slider.querySelector('.js-scroll-down');
+let currentSlide = 0;
+
+function slideLimiter(value) {
+    if (value >= slides.length) {
+        return 0;
+    } else if (value < 0) {
+        return slides.length - 1;
+    } else {
+        return value;
+    }
+}
+
+function fillSlider() {
+    mainSlide.src = slides[currentSlide].image;
+    slideTitle.innerText = slides[currentSlide].title;
+    slideDescription.innerText = slides[currentSlide].description;
+    slideLink.href = slides[currentSlide].link;
+    tags.innerHTML = '';
+
+    slides[currentSlide].tags.forEach(function (tag) {
+        const newElement = document.createElement('div.works__technology-item');
+
+        newElement.innerText = tag;
+        tags.appendChild(newElement);
+    });
+}
+
+upSlide.addEventListener('click', function () {
+    currentSlide = slideLimiter(currentSlide - 1);
+    fillSlider();
+});
+
+downSlide.addEventListener('click', function () {
+    currentSlide = slideLimiter(currentSlide + 1);
+    fillSlider();
+});
+
+fillSlider();
